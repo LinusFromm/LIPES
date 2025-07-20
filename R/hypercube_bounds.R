85946d0abfe459896b733ddce9c52f2dad8551bc
LinusFromm <linus.fromm18@gmail.com>
2025-07-21 09:34:07 +1200

    Added hypercube_bounds that finds minimum hypercuboid enclosing the fibre

--- R/hypercube_bounds.R
+++ R/hypercube_bounds.R
@@ -1,9 +1,18 @@
 #' Find the smallest bounds of the hypercube
 #' @param A Configuration matrix
 #' @param y Vector of observations
+#' @param CPLB_idx A2 index
 #'
 #' @return Vector a which defines the bounds of the hypercuboid
 #' @export
-hypercube_bounds <- function(A, y){
-  apply(A, 2, function(col) floor(min(y[which(col != 0)]/col[which(col != 0)])))
+hypercube_bounds <- function(A, CPLB_idx, y){
+  a = rep(0, length(CPLB_idx))
+  for(i in 1:length(CPLB_idx)){
+    a[i] = lpSolve::lp(direction = "max",
+                       objective.in = diag(ncol(A))[CPLB_idx[i],],
+                       const.mat = A,
+                       const.dir = "==",
+                       const.rhs = y)$solution[CPLB_idx[i]]
+  }
+  return(a)
 }
